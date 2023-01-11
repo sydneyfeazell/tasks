c37e9af62cb242f69cfbbc0b93568c163cf0c2fb
Sydney Feazell <sydneyfeazell@Sydneys-MacBook-Pro.local>
2023-01-11 12:45:26 -0500

    <sydneyfeazell> Task 01 upload b

--- Tasks/Tasks_01/task01.r
+++ Tasks/Tasks_01/task01.r
@@ -1,3 +1,23 @@
 # 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
 library(swirl)
+swirl()
+swirl()
 
+sydneyfeazell
+
+1
+
+5+7
+x <- 5+7
+x
+y <- x-3
+y
+z <- c(1.1,9,3.14)
+?c
+z
+c(z,555,z)
+z*2+1000
+my_sqrt <- sqrt(z-1)
+2
+my_div
+c(1,2,3,4)+c(0,10,100)
