560cf7c6bd2bb7fd688814a851c2a8fc3f48406d
sydneyfeazell <sf00002@mix.wvu.edu>
2023-01-17 12:36:48 -0500

    updating keychain + task01c

--- Tasks/Tasks_01/task01.r
+++ Tasks/Tasks_01/task01.r
@@ -1,4 +1,4 @@
-# ç
+# 
 library(swirl)
 swirl()
 swirl()
@@ -155,4 +155,24 @@ swirl()
 library(swirl)
 swirl()
 sydneyfeazell
+1
+data(cars)
+?cars
+head(cars)
+plot(cars)
+?plot
+plot(x=cars$speed, y=cars$dist)
+plot(x=cars$dist, y=cars$speed)
+plot(x=cars$speed, y=cars$dist,xlab="Speed")
+plot(x=cars$speed,y=cars$dist,ylab= "Stopping Distance")
+plot(x=cars$speed,y=cars$dist,ylab="Stopping Distance",xlab="Speed")
+plot(cars, main="My Plot")
+plot(cars, sub= "My Plot Subtitle")
+plot(cars,col=2)
+plot(cars,xlim= c(10,15))
+plot(cars, pch=2)
+data(mtcars)
+?boxplot
+boxplot(formula=mpg~cyl,data=mtcars)
+hist(mtcars$mpg)
 1
