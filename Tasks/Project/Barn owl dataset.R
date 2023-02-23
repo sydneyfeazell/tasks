fee23b60abb2b4dae875c896549d46a9306210af
sydneyfeazell <sf00002@mix.wvu.edu>
2023-02-23 12:26:54 -0500

    sydneyfeazell Task 05

--- Tasks/Project/Barn owl dataset.R
+++ Tasks/Project/Barn owl dataset.R
@@ -1,2 +1,6 @@
 setwd('~/Desktop/Evolution/Tasks/Project')
-barn owl<-read.csv("barn owl.csv")
+my_data <- read.table(pipe("pbpaste"), sep="\t", header=TRUE)
+head(my_data)
+
+#hypothesis
+#Will bill size increase as wing size increases in all three barn owl lineages?
